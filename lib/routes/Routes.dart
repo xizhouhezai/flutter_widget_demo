947bf595157dc9b4c4cd0cee70cfd5a7f0d4b764
xizhouhezai <yangrui1372106119@gmail.com>
2019-06-20 22:15:08 +0800

    add home page

---
+++ lib/routes/Routes.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../pages/Home.dart';
+
+final routes = {
+  '/': (context) => Home()
+};
+
+//固定写法
+var onGenerateRoute = (RouteSettings settings) {
+  // 统一处理
+  final String name = settings.name;
+  final Function pageContentBuilder = routes[name];
+  if (pageContentBuilder != null) {
+    if (settings.arguments != null) {
+      final Route route = MaterialPageRoute(
+        builder: (context) =>
+          pageContentBuilder(context, arguments: settings.arguments));
+      return route;
+    } else {
+      final Route route =
+        MaterialPageRoute(builder: (context) => pageContentBuilder(context));
+      return route;
+    }
+  }
+};
